a99b4988211f5c5c1f49fd981deb154dabf3b585
Marcus 'Loki' Jenz <marcus.jenz@googlemail.com>
2014-05-22 17:31:07 +0200

    implemented Class TurnImpl

---
+++ src/main/java/reversi/impl/TurnImpl.java
@@ -0,0 +1,53 @@
+package reversi.impl;
+
+import reversi.api.Color;
+import reversi.api.Turn;
+
+import java.util.Objects;
+
+/**
+ * Created by Loki on 22.05.2014.
+ */
+public class TurnImpl implements Turn{
+
+    private int x, y;
+    private Color color;
+
+    public TurnImpl(int x, int y, Color color)
+    {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    @Override
+    public int getX() {
+        return this.x;
+    }
+
+    @Override
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public Color getColor() {
+        return this.color;
+    }
+
+
+    public boolean equal(Object o)
+    {
+        if (o == null){
+            return false;
+        }
+        if(o instanceof Turn){
+            Turn t = (Turn) o;
+            if(t.getColor() == this.color && t.getX() == this.x && t.getY() == this.y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
